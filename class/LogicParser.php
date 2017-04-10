10a5510cdca9f115d9c7d2e5699df2aef2e31289
Hassan <bagheri.h@gmail.com>
2017-04-10 19:13:45 +0430

    LogicParse.php has been edited

--- class/LogicParser.php
+++ class/LogicParser.php
@@ -6,7 +6,7 @@
  * @copyright (c) 2017, Hans Burgman
  * @version 1.0.0
  */
-class logicParser{
+class LogicParser{
     /**
      *
      * @var string saved string on work 
@@ -103,27 +103,25 @@ class logicParser{
 	}
 	
 	//-------------------------------------------------------------
-	
-        /**
-         * get string from input and call strPArs method to parse it and return 
-         * its logical value.
-         * 
-         * @param string $string
-         * @return boolean
-         */
+	/**
+     * get string from input and call strPArs method to parse it and return 
+     * its logical value.
+     * 
+     * @param string $string
+     * @return boolean
+     */
 	public function logicCheck($string=NULL){
 		$this->setString($string);
-		echo $this->getString()."<br />";
 		$result = $this->strPars($this->getString());
 		return (intval($result)==1);
 	}
-	
 	//-------------------------------------------------------------
 	/**
          * Gets a part of string or a complex one to compare it logicaly. in case of 
-         * complex string this method will devide it to shortest string contains only 
-         * 2 boolean and a logic operator. it will takes from lastest and deepest open close
-         * prates.  
+         * complex string this method will devide it to shortest string contains only  
+         * deepest prateses that are not containes ane prantess inside. Then will
+         * start from last prantes and replace its logic value with it. untill no more
+         * prantess is inside of string. then compare result as a normal string  
          * @param string $str a string to check logical
          * @return boolean
          */
@@ -133,10 +131,9 @@ class logicParser{
 		if($subCount > 0){
 			for($i=$subCount-1 ; $i>=0 ; $i--){
 				$substr = $deepPrantesesStrings[0][$i][0];
-				$strlen = strlen($substr);//die("$substr $strlen"." ". substr($substr,1,$strlen-2)." " .__FILE__." ".__LINE__);
+				$strlen = strlen($substr);
 				$newSubstr = $this->clearStringParse(substr($substr,1,$strlen-2));
 				$str=substr_replace($str,$newSubstr,$deepPrantesesStrings[0][$i][1],$strlen);
-				echo "$str <br />";
 			}
 			$str = $this->strPars($str);
 		}
@@ -145,18 +142,38 @@ class logicParser{
 	}
 	
 	//-------------------------------------------------------------
-	private function getArrayWithPatern($patenr,$string){
-		preg_match_all('/\\([10&|!]+\\)/', $string, $matches, PREG_OFFSET_CAPTURE);
+	
+	/**
+	 * 
+	 * @param string $patern the patern that should search
+	 * @param The string that should be serch inside
+	 * @return array parent hapended as an array contains matches string and 
+	 * 		their pos
+	 */ 
+	
+	private function getArrayWithPatern($patern,$string){
+		preg_match_all($patern, $string, $matches, PREG_OFFSET_CAPTURE);
 		return $matches;
 	}
 	//-------------------------------------------------------------
 	
+	/**
+	 * Retuns array of deepest prantesses without any other prantesses inside
+	 * @param string $str string to search in
+	 * @return array parent hapended as an array contains matches string and 
+	 */
 	private function getDeepestPrantes($str){
 		$patern = "/\\([10&|!]+\\)/";
 		return $this->getArrayWithPatern($patern,$str);
 	}
 	//-------------------------------------------------------------
 	
+	/**
+	 * Checks a clear string that is not contains any pranteses
+	 * $param string $str String to parse
+	 * $return string a single charecter string that is 0 or 1 the result of
+	 * logical cheks of string
+	 */ 
 	private function clearStringParse($str){
 		$str = $this->checkNot($str);
 		
@@ -167,6 +184,12 @@ class logicParser{
 		return $str;
 	}
 	//-------------------------------------------------------------
+	/**
+	 * Check all NOT operators and replace values 
+	 * $param string $str string to check for NOT operator
+	 * @return string new string whiout NOT operator.
+	 */ 
+	
 	private function checkNot($str){
 		$notStrs = array("!!"=>"","!1"=>"0","!0"=>"1");
 		while(strpos($str,"!")!== FALSE){
@@ -179,28 +202,9 @@ class logicParser{
 	
 	//-------------------------------------------------------------
 	/**
-         * Takes tow parameters first is string second is a logical operator.
-         * Then will explode it in an array contaos of boolean values.
+         * Takes an string with and operators and replace all ANDS with thire value
          * @param string $string
-         * @param string $opr
-         * @return array
-         */
-	private function getBooleanArray($string,$opr){
-		$array = explode($opr,$string);
-		if(count($array)>2){
-			die("too long array on ".__FILE__." ".__LINE__);
-		}
-		foreach($array as $item){
-			$result[] = boolval($item); 
-		}
-		return $result;
-	}
-	
-	//-------------------------------------------------------------
-	/**
-         * Takes an string with and operator and check it if is true or false.
-         * @param string $string
-         * @return boolean
+         * @return string a new string without any AND operator
          */
 	private function CheckAnd($string){
 		$andRes = array("0&0"=>"0","0&1"=>"0","1&0"=>"0","1&1"=>"1");
@@ -214,9 +218,9 @@ class logicParser{
 	
 	//-------------------------------------------------------------
 	/**
-         * Takes an string with or operator and check it if is true or false.
+         * Takes an string with or operators and replace all ORs with thire value
          * @param string $string
-         * @return boolean
+         * @return string a new string without any OR operator
          */
 	private function CheckOr($string){
 		$orRes = array("0|0"=>"0","0|1"=>"1","1|0"=>"1","1&1"=>"1");
--- index.php
+++ index.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'class/logicParse.php';
+require_once 'class/LogicParser.php';
 
 $string = "1andFaLseoR(TRUEAnD0)|((((0|f)&0)|0|0)&1)|(1&(1&NOT~!0))";
-$stringParser = new logicParser();
+$stringParser = new LogicParser();
 
 $result = $stringParser->logicCheck($string);
 echo "</br >test $string: \n", ($result)?"TRUE":"FALSE";
